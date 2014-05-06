f81e85e01f1e93200ade32a4c3d21ae0f3f118f8
Jon Snider <jon@bowensnider.net>
2014-05-06 18:21:10 -0400

    Initial Commit

---
+++ oppisannoying/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  oppisannoying
+//
+//  Created by Jon Snider on 5/6/14.
+//  Copyright (c) 2014 orpheus.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
